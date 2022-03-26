4eb740450a9b1f123d3c8cb00b48f0759b41517f
Tom Tang <jjjj1997@live.hk>
2022-03-27 03:36:55 +0800

    feat: add react-intl, 18nv

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,29 @@
+import English from "../content/compiled-locales/en.json";
+import Chinese from "../content/compiled-locales/zh.json";
+
+import { MantineProvider } from "@mantine/core";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
+import { IntlProvider, FormattedMessage } from "react-intl";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
+
+  const { locale } = useRouter();
+  const [shortLocale] = locale ? locale.split("-") : ["en"];
+
+  const messages = useMemo(() => {
+    switch (shortLocale) {
+      case "zh":
+        return Chinese;
+      case "en":
+        return English;
+      default:
+        return English;
+    }
+  }, [shortLocale]);
 
   return (
     <>
@@ -23,7 +43,14 @@ export default function App(props: AppProps) {
           colorScheme: "light",
         }}
       >
-        <Component {...pageProps} />
+        <IntlProvider
+          locale={shortLocale}
+          messages={messages}
+          onError={() => null}
+        >
+          <FormattedMessage defaultMessage="Good Morning" id="greeting" />
+          <Component {...pageProps} />
+        </IntlProvider>
       </MantineProvider>
     </>
   );
