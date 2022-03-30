99500938508ea9b99594c0478fa83efebe5d2977
Tom Tang <jjjj1997@live.hk>
2022-03-31 03:32:17 +0800

    feat: add demo profile page, rooms page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,60 @@
+import { Container } from "@mantine/core";
 import type { NextPage } from "next";
+import { UserCardImage } from "../components/UserCard";
+
+export type UserProfile = {
+  image: string;
+  avatar: string;
+  name: string;
+  intro: string;
+  stats: { label: string; value: string }[];
+};
+
+// feature
+// view user join rooms, open rooms history, current rooms / queuing rooms
+
+// * Social
+// comment from other users -> comments sort by date
+// rating stars from other users -> rating score
+// contacts, add to contact / remove in contact (similar to friends)
+// comment state have public, private, hidden
+//
+
+const userData: UserProfile = {
+  image:
+    "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80",
+  avatar:
+    "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
+  name: "Bill Headbanger",
+  intro:
+    "Fullstack engineerFullstack engineerFullstack engineerFullstack engineerFullstack engineerFullstack engineerFullstack engineerFullstack engineerFullstack engineer",
+  stats: [
+    {
+      value: "34K",
+      label: "Followers",
+    },
+    {
+      value: "187",
+      label: "Follows",
+    },
+    {
+      value: "1.6K",
+      label: "Posts",
+    },
+  ],
+};
 
 const Profile: NextPage = () => {
-  return <div>Hello World Profile Page</div>;
+  return (
+    <>
+      <Container size="xs">
+        <UserCardImage {...userData} />
+
+        <div>房間 0</div>
+        <div>等候中 0</div>
+      </Container>
+    </>
+  );
 };
 
 export default Profile;
--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -1,9 +1,42 @@
+import { Container, SimpleGrid } from "@mantine/core";
 import type { NextPage } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
+import { useIntl } from "react-intl";
+import messages from "../lib/i18n/messages";
+import { StatsCard } from "./RoomPreview";
 
 const Rooms: NextPage = () => {
   const { query } = useRouter();
-  return <div>Hello World Rooms Page {JSON.stringify(query)} </div>;
+  const { formatMessage } = useIntl();
+  return (
+    <>
+      <Head>
+        <title>
+          {formatMessage(messages["header.links.rooms"])} | Famatch.io
+        </title>
+      </Head>
+
+      <Container size="xl">
+        <SimpleGrid
+          cols={4}
+          spacing="lg"
+          breakpoints={[
+            { maxWidth: "lg", cols: 3, spacing: "lg" },
+            { maxWidth: "md", cols: 3, spacing: "md" },
+            { maxWidth: "sm", cols: 2, spacing: "sm" },
+            { maxWidth: "xs", cols: 1, spacing: "sm" },
+          ]}
+        >
+          <StatsCard />
+          <StatsCard />
+          <StatsCard />
+          <StatsCard />
+          <StatsCard />
+        </SimpleGrid>
+      </Container>
+    </>
+  );
 };
 
 export default Rooms;
